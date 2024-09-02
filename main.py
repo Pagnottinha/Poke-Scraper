e04f3e9b0cab4ef4a8b844008db360042d4ba84b
Eduarda Medeiros <eduarda_medeiros_silva@hotmail.com>
2024-09-01 21:04:56 -0300

    implermentação evoluções e habilidades

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ### Onde vai ocorrer a junção dos csv
 import pandas as pd
+import ast
 import re
 
 # Função de extrair somente o valor em kg
@@ -16,14 +17,49 @@ def extract_cm(height_text):
         return str(round(float(match.group(1)) * 100, 1)) + ' cm' 
     return None
 
-df = pd.read_csv('./data.csv')
+#Função para combinar habilidades
+def combine_abilities(abilities):
+    combined = []
+    for ability in abilities:
+        url = ability['url']
+        name = ability['name'].strip()
+        description = ability['description'].strip()
+        combined.append({'url': url, 'name': name, 'description': description})
+    return combined
 
+#Agrupar por ID e combinar as habilidades
+def aggregate(group):
+    # Unir evoluções em uma lista de dicionários
+    evolutions = group['evolutions'].iloc[0] if not group['evolutions'].isnull().all() else []
+    
+    # Combinar habilidades
+    combined_abilities = []
+    for abilities in group['abilities']:
+        combined_abilities.extend(combine_abilities([abilities]))
+    
+    # Construir o resultado
+    result = {
+        'name': group['name'].iloc[0],
+        'url_pokemon': group['url_pokemon'].iloc[0],
+        'height': group['height'].iloc[0],
+        'weight': group['weight'].iloc[0],
+        'types': group['types'].iloc[0],
+        'evolutions': evolutions,
+        'abilities': combined_abilities
+    }
+    return result
+
+df = pd.read_csv('data.csv')
 df.dropna(inplace=True)
 
 df['height'] = df['height'].apply(extract_cm)
 df['weight'] = df['weight'].apply(extract_kg)
 
-df.columns = ['Numero', 'Nome', 'URL', 'Tamanho_em_CM', 'Peso_em_KG', 'Tipo']
+df['evolutions'] = df['evolutions'].apply(lambda x: ast.literal_eval(x))
+df['abilities'] = df['abilities'].apply(lambda x: ast.literal_eval(x))
 
-df.to_json('pokemon_data.json', orient='records', indent=4)
+# Aplicar agregação
+df_grouped = df.groupby('id').apply(aggregate).apply(pd.Series)
+df_grouped.to_json('pokemon_data.json', orient='records')
 
+print(df_grouped)
--- pokemon_base.py
+++ pokemon_base.py
@@ -3,25 +3,56 @@
 # http://pythonclub.com.br/material-do-tutorial-web-scraping-na-nuvem.html
 import scrapy
 
-class PokemonBaseScrapper(scrapy.Spider):
-  name = 'pokemon_base_scrapper'
-  domain = "https://pokemondb.net"
+class PokeSpider(scrapy.Spider):
+    name = 'pokemon_base_scrapper'
+    domain = "https://pokemondb.net"
 
-  start_urls = ["https://pokemondb.net/pokedex/all"]
+    start_urls = ["https://pokemondb.net/pokedex/all"]
 
-  def parse(self, response: scrapy.http.HtmlResponse):
-    pokemons = response.css('#pokedex > tbody > tr')
-    for pokemon in pokemons:
-    #pokemon = pokemons[0]
-      link = pokemon.css("td.cell-name > a::attr(href)").extract_first()
-      yield response.follow(self.domain + link, self.parse_pokemon)
+    def parse(self, response: scrapy.http.HtmlResponse):
+        pokemons = response.css('#pokedex > tbody > tr')
+        for pokemon in pokemons:
+            link = pokemon.css("td.cell-name > a::attr(href)").extract_first()
+            yield response.follow(self.domain + link, self.parse_pokemon)
 
-  def parse_pokemon(self, response: scrapy.http.HtmlResponse):
-    yield {
-      'id': response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(1) > td > strong::text').get(),
-      'name': response.css('#main > h1::text').get(),
-      'url': response.url,
-      'height': response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(4) > td::text').get(),
-      'weight': response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(5) > td::text').get(),
-      'types': response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(2) > td > a::text').getall()
-    }
+    def parse_pokemon(self, response: scrapy.http.HtmlResponse):
+        id = response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(1) > td > strong::text').get(),
+        name = response.css('#main > h1::text').get(),
+        url_pokemon = response.url,
+        height = response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(4) > td::text').get(),
+        weight = response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(5) > td::text').get(),
+        types = response.css('#main > .tabset-basics > .sv-tabs-panel-list > .active .vitals-table > tbody > tr:nth-child(2) > td > a::text').getall()
+        evolutions = response.css('div.infocard-list-evo > div.infocard')
+    
+        array_evolutions = []
+
+        for evolution in evolutions:
+            number = evolution.css('small::text').get()
+            name = evolution.css('a::text').get()
+            url = self.domain + evolution.css('a::attr(href)').get()
+
+            if number and name and url:
+                array_evolutions.append({"number": number, "name": name, "url": url})
+
+        abilities_urls = response.css('div:nth-child(1) > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > * > a::attr(href)').getall()    
+        for url in abilities_urls:
+            yield response.follow(self.domain + url, self.parse_abilities, meta={"id": id, "name": name, "url_pokemon": url_pokemon, "height": height, "weight": weight, "types": types,
+            "evolutions": array_evolutions})
+
+    def parse_abilities(self, response: scrapy.http.HtmlResponse):
+        name = response.css('#main > h1::text').get()
+        description = ''.join(response.css('div.grid-row > div:nth-child(1) > p::text').getall()).strip()
+    
+        yield {
+        "id" : response.meta["id"],
+        "name": response.meta["name"],
+        "url_pokemon": response.meta["url_pokemon"],
+        "height": response.meta["height"],
+        "weight": response.meta["weight"],
+        "types": response.meta["types"],
+        "evolutions": response.meta["evolutions"],
+        "abilities": {
+            "url": response.url,
+            "name": name,
+            "description": description
+        }}
